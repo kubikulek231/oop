d0ff2aae417d46243fb45c8558d7288b8eda10f1
Jakub Lepik <32817965+kubikulek231@users.noreply.github.com>
2023-03-01 19:07:26 +0100

    cv4 added

---
+++ cv4/cv4/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using cv4;
+using System.Diagnostics.Tracing;
+
+string string_input = "Toto je retezec predstavovany nekolika radky,\n"
+ + "ktere jsou od sebe oddeleny znakem LF (Line Feed).\n"
+ + "Je tu i nejaky ten vykricnik! Pro ucely testovani i otaznik?\n"
+ + "Toto je jen zkratka zkr. ale ne konec vety. A toto je\n"
+ + "posledni veta!"; 
+
+StringStatistics newStringStatistics = new StringStatistics(string_input);
+Console.WriteLine(newStringStatistics.getWordCount());
+Console.WriteLine(newStringStatistics.getSentenceCount());
+Console.WriteLine(newStringStatistics.getWordLongest());
+Console.WriteLine(newStringStatistics.getWordShortest());
+Console.WriteLine(newStringStatistics.getWordMostOccuring());
+foreach (string word in newStringStatistics.getWordSort())
+{
+    Console.WriteLine(word);
+}
---
+++ cv4/cv4/StringStatistics.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace cv4
+{
+    internal class StringStatistics
+    {
+        private string input_string { get; set; }
+
+        public StringStatistics(string input)
+        {
+            this.input_string = input;
+        }
+
+        public int getWordCount()
+        {
+            char[] slicers = { ',', ' ', '.', '?', '.', '!', '\n' };
+            string[] words = input_string.Split(slicers, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+            return words.Length;
+        }
+        public int getRowCount()
+        {
+            int count = 0;
+            foreach (char c in this.input_string)
+            {
+                if (c == '\n')
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        public int getSentenceCount()
+        {
+            int count = -1;
+            bool upperCase = false;
+            foreach (char c in input_string)
+            {
+                if (char.IsUpper(c) && (char.IsLetter(c)))
+                {
+                    upperCase = true;
+                }
+                if (upperCase && (c == '.' || c == '?' || c == '!')) {
+                    count++;
+                }
+            }
+            return count;
+        }
+        public StringBuilder getWordLongest()
+        {
+            string longest_word = "";
+            StringBuilder longest_words = new StringBuilder{};
+
+            char[] slicers = { ',', ' ', '.', '?', '.', '!', '\n' };
+            string[] words = input_string.Split(slicers, StringSplitOptions.RemoveEmptyEntries);
+            
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length > longest_word.Length)
+                {
+                    longest_word = words[i];
+                }
+            }
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length == longest_word.Length)
+                {
+                    longest_words.AppendLine(words[i]);
+                }
+            }
+            return longest_words;
+        }
+        public StringBuilder getWordShortest()
+        {
+            char[] slicers = { ',', ' ', '.', '?', '.', '!', '\n' };
+            string[] words = input_string.Split(slicers, StringSplitOptions.RemoveEmptyEntries);
+            string shortest_word = words[0];
+            StringBuilder shortest_words = new StringBuilder { };
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length < shortest_word.Length)
+                {
+                    shortest_word = words[i];
+                }
+            }
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length == shortest_word.Length)
+                {
+                    shortest_words.AppendLine(words[i]);
+                }
+            }
+            return shortest_words;
+        }
+    
+
+        public string getWordMostOccuring()
+        {
+            char[] slicers = { ',', ' ', '.', '?', '.', '!', '\n' };
+            string[] words = input_string.Split(slicers, StringSplitOptions.RemoveEmptyEntries);
+            int[] occurances = new int[words.Length];
+            for (int i = 0; i < occurances.Length; i++)
+            {
+                for (int j = 0; i < occurances.Length; i++)
+                {
+                    if (words[i] == words[j])
+                    {
+                        occurances[i]++;
+                    }
+                }
+            }
+            int biggest = 0;
+            int biggest_index = 0;
+            for (int i = 0; i < occurances.Length; i++)
+            {
+                if (occurances[i] > biggest)
+                {
+                    biggest = occurances[i];
+                    biggest_index = i;
+                }
+
+            }
+
+            return words[biggest_index];
+        }
+        public string[] getWordSort()
+        {
+            char[] slicers = { ',', ' ', '.', '?', '.', '!', '\n' };
+            string[] words = input_string.Split(slicers, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i].Replace(")", string.Empty);
+                words[i] = words[i].Replace("(", string.Empty);
+            }
+            Array.Sort<string>(words);
+            return words;
+        }
+    }
+}
